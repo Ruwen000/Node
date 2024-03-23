ef99e6b49b0afcb5df6bb41079404e7aee741c30
Leonhard Schaefler <leonhard.schaefler@rwu.de>
2024-03-23 11:34:59 +0100

    change cores

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const crypto = require('crypto');
 const pkg = require('./package.json');
-
+const fs = require('fs');
 
 // App constants
 const port = process.env.PORT || 3000;
@@ -18,7 +18,7 @@ const apiPrefix = '/api';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/}));
+app.use(cors());
 app.options('*', cors());
 
 // ***************************************************************************
@@ -32,14 +32,48 @@ app.post('/', function (req, res) {
 })
 
 app.get('/img', function (req, res) {
-    return res.send("Fabrikam Bank API");
-})
+  // Assuming the file you saved earlier is named 'data.json'
+  const filePath = 'data.json';
+
+  // Read the file from disk
+  fs.readFile(filePath, 'utf8', function (err, data) {
+      if (err) {
+          console.error('Error reading file:', err);
+          return res.status(500).send('Error reading file.');
+      }
+
+      // Parse the JSON data
+      const jsonData = JSON.parse(data);
+
+      // Send the JSON data as response
+      return res.json(jsonData);
+  });
+});
+
+app.post('/img', function (req, res) {
+  // Get JSON data from request body
+  const jsonData = req.body;
+
+  // Assuming you want to save the JSON data to a file named 'data.json'
+  const filePath = 'data.json';
+
+  // Write the JSON data to the file
+  fs.writeFile(filePath, JSON.stringify(jsonData), 'utf8', function (err) {
+      if (err) {
+          console.error('Error saving JSON data to file:', err);
+          return res.status(500).send('Error saving JSON data to file.');
+      }
+      console.log('JSON data saved to file:', filePath);
+      return res.send('JSON data saved successfully.');
+  });
+});
+
 
 app.delete('/img/:id', function (req, res) {
   return res.send("Fabrikam Bank API");
 })
 
-app.delete('/img', function (req, res) {
+app.delete('/imga', function (req, res) {
   return res.send("Fabrikam Bank API");
 })
 
